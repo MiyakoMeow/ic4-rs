69de867dc1c081a4ead48e5ba032b1e9401c2bb8
MiyakoMeow <miyakomeow@qq.com>
2024-10-08 16:36:50 +0800

    Modify tests.

--- ic4-sys/tests/tests_sys_general.rs
+++ ic4-sys/tests/tests_sys_general.rs
@@ -1,27 +1,7 @@
-use std::ffi::CStr;
+use core::panic;
+use std::ffi::{c_char, CStr, CString};
 
 use ic4_sys::*;
-
-/*
- * Tools func
- */
-
-fn print_device_info(device_info: *const IC4_DEVICE_INFO) {
-    unsafe {
-        println!(
-            "Model: {} ",
-            CStr::from_ptr(ic4_devinfo_get_model_name(device_info)).to_string_lossy()
-        );
-        println!(
-            "Serial: {}",
-            CStr::from_ptr(ic4_devinfo_get_serial(device_info)).to_string_lossy()
-        );
-        println!(
-            "Version: {}",
-            CStr::from_ptr(ic4_devinfo_get_version(device_info)).to_string_lossy()
-        );
-    }
-}
 
 /*
  * Tests
@@ -44,7 +24,7 @@ fn enumerate_devinfo() {
 
         let count = ic4_devenum_get_device_count(enumerator);
         if count == 0 {
-            println!("No devices found\n");
+            eprintln!("No devices found.\n");
             return;
         }
 
@@ -62,43 +42,135 @@ fn enumerate_devinfo() {
 }
 
 #[test]
-fn connect_camera() {
+fn grabbing_image() {
+    use ic4_sys::*;
     unsafe {
-        ic4_init_library(std::ptr::null());
+        ic4_init_library(std::ptr::null_mut());
 
         // Create a grabber object
         let mut grabber = std::ptr::null_mut();
         if !ic4_grabber_create(std::ptr::from_mut(&mut grabber)) {
-            panic!("Failed to create grabber")
+            print_last_error();
+            panic!("Failed when executing ic4_grabber_create");
         }
 
         // Find the first available video capture device
         let mut enumerator = std::ptr::null_mut();
         if !ic4_devenum_create(std::ptr::from_mut(&mut enumerator)) {
-            panic!("Failed to create devenum")
+            print_last_error();
+            panic!("Failed when executing ic4_devenum_create");
         }
+        ic4_devenum_update_device_list(enumerator);
 
-        if !ic4_devenum_update_device_list(enumerator) {
-            panic!("Failed to update devenum")
-        }
-        let mut info = std::ptr::null_mut();
         let count = ic4_devenum_get_device_count(enumerator);
-
-        #[cfg(not(feature = "test-ensure-existing-device"))]
         if count == 0 {
+            eprintln!("No devices found.\n");
             return;
         }
 
+        let mut info = std::ptr::null_mut();
         if !ic4_devenum_get_devinfo(enumerator, 0, std::ptr::from_mut(&mut info)) {
-            panic!("Failed to get devinfo!")
+            print_last_error();
+            panic!("Failed when executing ic4_devenum_get_devinfo");
         }
         ic4_devenum_unref(enumerator);
 
         // Open it
         if !ic4_grabber_device_open(grabber, info) {
-            panic!("Failed to open device")
+            ic4_devinfo_unref(info);
+            print_last_error();
+            panic!("Failed to open device!");
         }
         ic4_devinfo_unref(info);
-        ic4_exit_library();
+
+        let mut map = std::ptr::null_mut();
+        if !ic4_grabber_device_get_property_map(grabber, std::ptr::from_mut(&mut map)) {
+            print_last_error();
+            panic!("Failed when executing ic4_grabber_device_get_property_map");
+        }
+
+        // Set the resolution to 640x480
+        if !ic4_propmap_set_value_int64(map, b"Width\0".as_ptr() as *const c_char, 640) {
+            print_last_error();
+            panic!("Failed when executing ic4_propmap_set_value_int64");
+        }
+        if !ic4_propmap_set_value_int64(map, b"Height\0".as_ptr() as *const c_char, 480) {
+            print_last_error();
+            panic!("Failed when executing ic4_propmap_set_value_int64");
+        }
+
+        ic4_propmap_unref(map);
+
+        let mut sink = std::ptr::null_mut();
+        if !ic4_snapsink_create(std::ptr::from_mut(&mut sink), std::ptr::null_mut()) {
+            print_last_error();
+            panic!("Failed when executing ic4_snapsink_create");
+        }
+
+        if !ic4_grabber_stream_setup(grabber, sink, std::ptr::null_mut(), true) {
+            print_last_error();
+            panic!("Failed to open stream!");
+        }
+
+        let mut buffer = std::ptr::null_mut();
+        if !ic4_snapsink_snap_single(sink, std::ptr::from_mut(&mut buffer), 1000) {
+            print_last_error();
+            panic!("Failed to get image buffer\n");
+        }
+
+        if !ic4_imagebuffer_save_as_bmp(
+            buffer,
+            CStr::from_bytes_with_nul_unchecked(b"test.bmp").as_ptr(),
+            std::ptr::from_ref(&IC4_IMAGEBUFFER_SAVE_OPTIONS_BMP {
+                store_bayer_raw_data_as_monochrome: 0,
+            }),
+        ) {
+            print_last_error();
+            panic!("Failed to save image buffer\n");
+        }
+
+        ic4_grabber_stream_stop(grabber);
+    }
+}
+
+/*
+ * Tools func
+ */
+
+fn print_device_info(device_info: *const IC4_DEVICE_INFO) {
+    unsafe {
+        println!(
+            "Model: {} ",
+            CStr::from_ptr(ic4_devinfo_get_model_name(device_info)).to_string_lossy()
+        );
+        println!(
+            "Serial: {}",
+            CStr::from_ptr(ic4_devinfo_get_serial(device_info)).to_string_lossy()
+        );
+        println!(
+            "Version: {}",
+            CStr::from_ptr(ic4_devinfo_get_version(device_info)).to_string_lossy()
+        );
+    }
+}
+
+fn print_last_error() {
+    unsafe {
+        let mut error = ic4_sys::IC4_ERROR::IC4_ERROR_UNKNOWN;
+        let mut message_length = 10 * 1024;
+        let mut message_vec: Vec<_> = std::iter::repeat(0u8).take(message_length).collect();
+        if !ic4_sys::ic4_get_last_error(
+            std::ptr::from_mut(&mut error),
+            message_vec.as_mut_ptr() as *mut c_char,
+            std::ptr::from_mut(&mut message_length),
+        ) {
+            panic!("Failed to get error!")
+        }
+        message_vec.resize(message_length - 1, 0); // Remove tailing '\0'
+        println!(
+            "{:?} {}",
+            error,
+            CString::from_vec_with_nul_unchecked(message_vec).to_string_lossy()
+        );
     }
 }
--- ic4/tests/tests_general.rs
+++ ic4/tests/tests_general.rs
@@ -1,12 +1,16 @@
-use std::{ffi::CStr, ptr::null_mut, thread::sleep, time::Duration};
+use std::{ffi::CStr, ptr::null_mut};
 
 use ic4::*;
 
 #[test]
 fn enumerate_device() -> ic4::Result<()> {
+    // ic4::exit_library();
     ic4::init_library(&InitConfig::default_ext())?;
     let mut dev_enum = DeviceEnum::create()?;
+    dev_enum.update_device_list()?;
+    dev_enum.update_interface_list()?;
     let device_count = dev_enum.get_device_count();
+    eprintln!("Device Count: {device_count}");
     #[cfg(not(feature = "test-ensure-existing-device"))]
     if device_count == 0 {
         return Ok(());
@@ -14,30 +18,43 @@ fn enumerate_device() -> ic4::Result<()> {
     for device_index in 0..device_count {
         let device = dev_enum.get_device_info(device_index as usize)?;
         let device_name = device.get_model_name();
-        println!("Model Name: {}", device_name.to_string_lossy());
+        eprintln!("Model Name: {}", device_name.to_string_lossy());
     }
+    drop(dev_enum);
     ic4::exit_library();
     Ok(())
 }
 
 #[test]
 fn get_image() -> ic4::Result<()> {
+    // ic4::exit_library();
     ic4::init_library(&InitConfig::default_ext())?;
-    let mut dev_enum = DeviceEnum::create()?;
-    let device_count = dev_enum.get_device_count();
-    #[cfg(not(feature = "test-ensure-existing-device"))]
-    if device_count == 0 {
-        return Ok(());
-    }
     let mut grabber = Grabber::create()?;
-    let mut dev_info = dev_enum.get_device_info(0)?;
-    grabber.device_open(&mut dev_info)?;
+    {
+        let mut dev_enum = DeviceEnum::create()?;
+        dev_enum.update_device_list()?;
+        dev_enum.update_interface_list()?;
+        #[cfg(not(feature = "test-ensure-existing-device"))]
+        let device_count = dev_enum.get_device_count();
+        #[cfg(not(feature = "test-ensure-existing-device"))]
+        if device_count == 0 {
+            return Ok(());
+        }
+        let mut dev_info = dev_enum.get_device_info(0)?;
+        grabber.device_open(&mut dev_info)?;
+    }
+
+    let mut prop_map = grabber.device_get_property_map()?;
+    prop_map.set_value_i64(PropertyName::Width, 640)?;
+    prop_map.set_value_i64(PropertyName::Height, 480)?;
+
+    eprintln!("Device Open!");
 
     let mut snap_sink = SnapSink::create(&SnapSinkConfig {
         strategy: SnapSinkAllocationStrategy::IC4_SNAPSINK_ALLOCATION_STRATEGY_DEFAULT,
-        num_buffers_alloc_on_connect: 100,
-        num_buffers_allocation_threshold: 100,
-        num_buffers_free_threshold: 100,
+        num_buffers_alloc_on_connect: 1,
+        num_buffers_allocation_threshold: 1,
+        num_buffers_free_threshold: 1,
         num_buffers_max: 300,
         pixel_formats: std::ptr::from_ref(&PixelFormat::IC4_PIXEL_FORMAT_BGR8),
         num_pixel_formats: 0,
@@ -53,8 +70,13 @@ fn get_image() -> ic4::Result<()> {
         WindowHandle::from(ic4_sys::IC4_WINDOW_HANDLE(null_mut())),
     )?;
     grabber.stream_setup(snap_sink.as_mut(), &mut display, false)?;
-    sleep(Duration::from_millis(1000));
-    let mut image = snap_sink.snap_single(100)?;
+
+    eprintln!("SnapSink setup!");
+
+    let mut image = snap_sink.snap_single(10000)?;
+
+    eprintln!("SnapSink get buffer!");
+
     image.save_as_tiff(
         unsafe { CStr::from_bytes_with_nul_unchecked(&b"./test/result.tiff"[..]) },
         &ImageBufferSaveOptionsTIFF::default_ext(),
